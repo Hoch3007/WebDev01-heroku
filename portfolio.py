ec1c117dedbf5ea32d82c059ffc89e0af538f425
Ricarda <wunderpott@gmail.com>
2019-03-18 09:33:08 +0100

    changed for exercise

--- portfolio.py
+++ portfolio.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, flash, make_response
+from random import randint
+
 
 app = Flask(__name__)
-
+app.secret_key = '6ca97bc342cdbecabbadb5c47b006ef4'
 
 posts = [
             {'author' : 'Hoch3007', 'title' : 'Blog Post 1', 'content' :'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 'date_posted': 'March, 1st 2019'},
     {'author': 'Hoch3007', 'title': 'Blog Post 2',
      'content': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-     'date_posted': 'March, 2st 2019'}
+     'date_posted': 'March, 2nd 2019'},
+    {'author': 'Hoch3007', 'title': 'Blog Post 3',
+     'content': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+     'date_posted': 'April, 1st 2019'},
+{'author': 'Hoch3007', 'title': 'Blog Post 4',
+     'content': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+     'date_posted': 'May, 21st 2019'},
+{'author': 'Hoch3007', 'title': 'Blog Post 5',
+     'content': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+     'date_posted': 'June, 12th 2019'}
 ]
 
 
@@ -26,18 +37,65 @@ def portfolio():
     return render_template('portfolio.html')
 
 
-@app.route("/portfolio/fakebook")
-def fakebook():
-    return "Fakebook"
-
-
-@app.route("/portfolio/boogle")
-def boogle():
-    return "Boogle"
-
 @app.route("/blog")
 def blog():
     return render_template('blog.html', posts=posts)
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    contact_name = request.form.get("name")
+    contact_text = request.form.get("text_message")
+
+    print(contact_name)
+    print(contact_text)
+
+    if request.method == "POST":
+        response = make_response(render_template("success.html"))
+        response.set_cookie("user_name", contact_name)
+        return response
+    else:
+        user_name = request.cookies.get("user_name")
+        return render_template('contact.html', name=user_name)
+
+
+@app.route("/game", methods=["GET", "POST"])
+def guess():
+
+    number_guess = request.form.get("guess")
+
+    if request.method == "POST":
+
+        secret_number = request.cookies.get("secret_number")
+
+        # flask app akzeptiert bei Cookies keine Integer
+        if int(secret_number) == int(number_guess):
+            response.set_cookie("secret_number", expires=0)
+            return render_template("result.html")
+
+        else:
+
+            if int(secret_number) > int(number_guess):
+                direction = "higher"
+            else:
+                direction = "lower"
+
+            return render_template("error.html", direction=direction)
+
+    elif request.method == "GET":
+
+        secret_number = request.cookies.get("secret_number")
+
+        if secret_number is None:
+
+            number = randint(0, 30)
+            response = make_response(render_template("game.html"))
+            response.set_cookie("secret_number", str(number))
+
+            return response
+        else:
+
+            return render_template('game.html')
 
 
 if __name__ == '__main__':
